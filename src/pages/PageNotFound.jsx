d2b407abbec14a143f69f29dde71d2c7fe892301
dev-krishnadeshpande <krishnadeshpande.dev@gmail.com>
2025-01-01 19:23:55 +0530

    Added page not found page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,19 @@
+import { useMoveBack } from "../hooks/useMoveBack";
+import "./page-not-found.css";
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  const moveBack = useMoveBack();
+
+  return (
+    <main className="page-not-found-container">
+      <div className="page-not-found-content">
+        <h1>The page you are looking for could not be found 😢</h1>
+        <button onClick={moveBack} size="large">
+          &larr; Go back
+        </button>
+      </div>
+    </main>
+  );
 };
 
 export default PageNotFound;
