0496280073f21ee38e8b496787f7b608a6145a5b
dev-krishnadeshpande <krishnadeshpande.dev@gmail.com>
2025-01-01 19:22:08 +0530

    Added generic modal using compound component

---
+++ src/ui/Modal.jsx
@@ -0,0 +1,51 @@
+import { cloneElement, createContext, useContext } from "react";
+import "./modal.css";
+import { useState } from "react";
+import { useOutsideClick } from "../hooks/useOutsideClick";
+import { createPortal } from "react-dom";
+import { HiXMark } from "react-icons/hi2";
+
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+
+  const close = () => setOpenName("");
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
+  if (name !== openName) return null;
+
+  return createPortal(
+    <div className="modal-overlay">
+      <div className="modal-content" ref={ref}>
+        <button className="modal-close-btn" onClick={close}>
+          <HiXMark />
+        </button>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
+      </div>
+    </div>,
+    document.body
+  );
+}
+
+Modal.Open = Open;
+Modal.Window = Window;
+
+export default Modal;
